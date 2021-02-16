4e59cbc39fa4cb085e5c2b512a3c5f07f475d659
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-02-16 21:33:45 +0100

    Add some MD5 and SHA1 hashing utilities

---
+++ Fluffle.Utils/Hashing.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Noppes.Fluffle.Utils
+{
+    /// <summary>
+    /// Provides some convenient ways to generate MD5 and SHA1 hashes.
+    /// </summary>
+    public static class Hashing
+    {
+        /// <summary>
+        /// Computes the MD5 hash of the provided ASCII encoded string.
+        /// </summary>
+        public static string Md5(string value)
+        {
+            var bytes = Encoding.ASCII.GetBytes(value);
+
+            return Md5(bytes);
+        }
+
+        /// <summary>
+        /// Computes the MD5 hash for the given array of bytes.
+        /// </summary>
+        public static string Md5(byte[] bytes)
+        {
+            using var md5 = MD5.Create();
+
+            return BytesToString(md5.ComputeHash(bytes));
+        }
+
+        /// <summary>
+        /// Computes the SHA1 hash for the given stream.
+        /// </summary>
+        public static string Sha1(Stream stream)
+        {
+            using var sha1 = SHA1.Create();
+
+            return BytesToString(sha1.ComputeHash(stream));
+        }
+
+        private static string BytesToString(ReadOnlySpan<byte> bytes)
+        {
+            var hashBuilder = new StringBuilder();
+
+            foreach (var part in bytes)
+                hashBuilder.Append(part.ToString("x2"));
+
+            return hashBuilder.ToString();
+        }
+    }
+}
