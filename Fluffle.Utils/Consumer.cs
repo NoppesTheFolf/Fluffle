8186aa4864ab0ceeadd283f5ad21aaf787b85559
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-02-17 16:43:45 +0100

    Add classes to help dealing with multithreaded data processing

---
+++ Fluffle.Utils/Consumer.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Noppes.Fluffle.Utils
+{
+    /// <summary>
+    /// Base class for all consumers. Also functions as a <see cref="Producer{T}"/> if the consumer
+    /// has an output to write to.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Consumer<T> : Producer<T>
+    {
+        public ChannelReader<T> Input { get; set; }
+
+        public override async Task WorkAsync()
+        {
+            var input = await Input.ReadAsync();
+
+            var output = await ConsumeAsync(input);
+            await ProduceAsync(output);
+        }
+
+        public abstract Task<T> ConsumeAsync(T toConsume);
+    }
+}
