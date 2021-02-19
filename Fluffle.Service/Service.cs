9e3a6244ea8ca79fb415fd5dc0cd1a8d525b6c26
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-02-19 21:17:01 +0100

    Add service project

---
+++ Fluffle.Service/Service.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Noppes.Fluffle.Configuration;
+using Serilog;
+using System;
+using System.Threading.Tasks;
+
+namespace Noppes.Fluffle.Service
+{
+    public abstract class Service
+    {
+        protected internal IServiceProvider Services { get; set; } = null!;
+
+        protected internal abstract Task RunAsync();
+    }
+
+    public abstract class Service<TService> : Service where TService : Service
+    {
+        public static async Task RunAsync(Func<TService, FluffleConfiguration, IServiceCollection, Task> configureServicesAsync = null, Func<TService, Task> configureAsync = null)
+        {
+            var service = Activator.CreateInstance<TService>();
+
+            var configuration = FluffleConfiguration.Load<TService>();
+
+            var serviceCollection = new ServiceCollection();
+
+            if (configureServicesAsync != null)
+                await configureServicesAsync(service, configuration, serviceCollection);
+
+            service.Services = serviceCollection.BuildServiceProvider();
+
+            if (configureAsync != null)
+                await configureAsync(service);
+
+            try
+            {
+                await service.RunAsync();
+            }
+            catch (Exception exception)
+            {
+                Log.Fatal(exception, "Oh no! An non-transient exception occurred! This probably indicates there is a bug.");
+            }
+        }
+    }
+}
