0c24bdeab743c3a569f4ba440e1fb335f724120b
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-02-20 15:25:49 +0100

    Add mapping functionality

---
+++ Fluffle.Api/Mapping/MapperMissingPublicConstructorException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Noppes.Fluffle.Api.Mapping
+{
+    public class MapperMissingPublicConstructorException : Exception
+    {
+        public MapperMissingPublicConstructorException(Type type)
+            : base($"Mapper of type {type.Name} must have a public parameterless constructor.")
+        {
+        }
+    }
+}
---
+++ Fluffle.Api/Mapping/TypeDoesNotImplementMapperException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Noppes.Fluffle.Api.Mapping
+{
+    public class TypeDoesNotImplementMapperException : Exception
+    {
+        public TypeDoesNotImplementMapperException(Type type)
+            : base($"Type {type.Name} does not implement {typeof(IMapper<,>).Name}")
+        {
+        }
+    }
+}
