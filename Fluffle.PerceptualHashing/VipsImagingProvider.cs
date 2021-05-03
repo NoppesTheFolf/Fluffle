1f58467e87df57e9b41f5a25288f22d8000600a3
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-05-03 18:35:25 +0200

    Add caching to the libvips imaging provider

--- Fluffle.PerceptualHashing/VipsImagingProvider.cs
+++ Fluffle.PerceptualHashing/VipsImagingProvider.cs
@@ -3,6 +3,8 @@ using Nitranium.PerceptualHashing.Imaging;
 using Nitranium.PerceptualHashing.Imaging.Ppm;
 using Nitranium.PerceptualHashing.Utils;
 using Noppes.Fluffle.Vips;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Noppes.Fluffle.PerceptualHashing
 {
@@ -11,13 +13,40 @@ namespace Noppes.Fluffle.PerceptualHashing
     /// </summary>
     public class VipsImagingProvider : FileImagingProvider
     {
+        private readonly SemaphoreSlim _mutex;
+        private readonly IDictionary<Resolution, TemporaryFile> _cache;
+
+        public VipsImagingProvider()
+        {
+            _mutex = new SemaphoreSlim(1);
+            _cache = new Dictionary<Resolution, TemporaryFile>();
+        }
+
         public override Image Prepare(string inputPath, in Resolution resolution)
         {
-            using var temporaryFile = new TemporaryFile();
+            _mutex.Wait();
+            try
+            {
+                if (!_cache.TryGetValue(resolution, out var temporaryFile))
+                {
+                    temporaryFile = new TemporaryFile();
+                    FluffleVips.ThumbnailPpm(inputPath, temporaryFile.Location, resolution.Width, resolution.Height);
 
-            FluffleVips.ThumbnailPpm(inputPath, temporaryFile.Location, resolution.Width, resolution.Height);
+                    _cache[resolution] = temporaryFile;
+                }
 
-            return new PpmReader(temporaryFile.Location);
+                return new PpmReader(temporaryFile.Location);
+            }
+            finally
+            {
+                _mutex.Release();
+            }
+        }
+
+        public override void Dispose()
+        {
+            foreach (var temporaryFile in _cache.Values)
+                temporaryFile.Dispose();
         }
     }
 
