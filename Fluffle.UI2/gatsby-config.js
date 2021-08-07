64e5796a9b13ed3a8a886c98ae753c109dc411f1
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-08-07 13:22:10 +0200

    Add Gatsby template

---
+++ Fluffle.UI2/gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://fluffle.xyz",
+    title: "Fluffle",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
