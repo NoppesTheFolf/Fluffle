fbc1bbee7223585f686d22a354d636e3b76803c1
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-08-10 14:28:19 +0200

    Add contact page

--- Fluffle.UI2/src/components/icon.tsx
+++ Fluffle.UI2/src/components/icon.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react'
 import Svg from '../services/svg'
 
-const Icon = ({ name }: { name: string }) => {
+const Icon = ({ name, inheritSize }: { name: string, inheritSize: boolean }) => {
     return (
-        <span className="flex" dangerouslySetInnerHTML={{ __html: Svg.getRaw(name) }} />
+        <span className="flex" style={{ fontSize: inheritSize ? 'inherit' : '24px' }} dangerouslySetInnerHTML={{ __html: Svg.getRaw(name) }} />
     )
 }
+
+Icon.defaultProps = {
+    inheritSize: false
+};
 
 export default Icon
--- Fluffle.UI2/src/pages/contact.tsx
+++ Fluffle.UI2/src/pages/contact.tsx
@@ -1,10 +1,49 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
+import Icon from '../components/icon'
+
+const artistName = 'Oggy123'
+const artUrl = 'https://twitter.com/OggyOsbourne/status/1294700124187262976'
+
+const socials = [
+    { name: 'Telegram', url: 'https://t.me/NoppesTheFolf', icon: 'telegram-plane' },
+    { name: 'Twitter', url: 'https://twitter.com/NoppesTheFolf', icon: 'twitter' },
+    { name: 'GitHub', url: 'https://github.com/NoppesTheFolf', icon: 'github-alt' }
+];
+
+const SocialBadge = ({ social }) => {
+    return (
+        <a className="transition-colors text-4xl fill-light-100 hover:fill-dark-400 hover:bg-light-100 p-3 rounded-full border-2" href={social.url} target="_blank" rel="noreferrer">
+            <Icon inheritSize={true} name={social.icon} />
+        </a>
+    )
+}
 
 const ContactPage = () => {
     return (
         <Layout center={true} title="Contact">
-            Contact
+            <div className="flex flex-col gap-y-6 text-center">
+                <div>
+                    <a href={artUrl} target="_blank" rel="noreferrer">
+                        <StaticImage alt={`NoppesTheFolf icon by ${artistName}`} className="w-64 rounded-full" src="../images/noppes.jpg" />
+                    </a>
+                    <div className="italic">
+                        <span className="text-muted">Art by </span>
+                        <a href={artUrl} target="_blank" rel="noreferrer">{artistName}</a>
+                    </div>
+                </div>
+                <div>
+                    <div className="w-72 sm:w-full">
+                        Found a bug? Got a feature request? Contact a folf.
+                    </div>
+                </div>
+                <div className="flex flex-wrap justify-center gap-12">
+                    {socials.map(s => (
+                        <SocialBadge key={s.name} social={s}></SocialBadge>
+                    ))}
+                </div>
+            </div>
         </Layout>
     )
 }
