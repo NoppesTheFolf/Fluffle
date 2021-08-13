97d3d8c20c6d13d12f51deef0d697ab23af639b8
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-08-13 22:00:50 +0200

    Fix icons not scaling with font size

--- Fluffle.UI2/src/components/icon.tsx
+++ Fluffle.UI2/src/components/icon.tsx
@@ -3,7 +3,7 @@ import Svg from '../services/svg'
 
 const Icon = ({ name, inheritSize }: { name: string, inheritSize: boolean }) => {
     return (
-        <span className="flex" style={{ fontSize: inheritSize ? 'inherit' : '24px' }} dangerouslySetInnerHTML={{ __html: Svg.getRaw(name) }} />
+        <span className="flex" style={{ fontSize: inheritSize ? 'inherit' : '1.5em' }} dangerouslySetInnerHTML={{ __html: Svg.getRaw(name) }} />
     )
 }
 
