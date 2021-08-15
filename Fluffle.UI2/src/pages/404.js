ae9b7799a31d7c837dac08ee7f8e32823e093cb2
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-08-15 18:52:29 +0200

    Fix 404 layout

--- Fluffle.UI2/src/pages/404.js
+++ Fluffle.UI2/src/pages/404.js
@@ -4,8 +4,10 @@ import Layout from '../components/layout'
 const NotFoundPage = () => {
     return (
         <Layout center={true} title="Not found">
-            <div className="text-7xl font-light mb-4">Blimey! 404 Not Found</div>
-            <div className="text-muted text-3xl font-light">We couldn't find the page you're looking for</div>
+            <div className="flex flex-col items-center text-center">
+                <div className="text-4xl sm:text-7xl font-light mb-4">Blimey! 404 Not Found</div>
+                <div className="text-muted text-xl sm:text-3xl font-light">We couldn't find the page you're looking for</div>
+            </div>
         </Layout>
     )
 }
