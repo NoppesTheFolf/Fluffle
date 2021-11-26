355b04598caac90dc3d00fbca88c94a2317c350e
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2021-11-26 15:31:39 +0100

    Bump version to 0.11.2

--- Fluffle.Bot/config.py
+++ Fluffle.Bot/config.py
@@ -3,7 +3,7 @@ import yaml
 
 
 def get_version() -> str:
-    return '0.11.1' if os.environ.get('DEV') is None else 'development'
+    return '0.11.2' if os.environ.get('DEV') is None else 'development'
 
 
 def load() -> dict:
