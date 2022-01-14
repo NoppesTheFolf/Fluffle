c56841ded180229d4e10ea3c70f1f2e660600483
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2022-01-14 19:43:13 +0100

    Bump version to 0.14.0

--- Fluffle.Bot/config.py
+++ Fluffle.Bot/config.py
@@ -27,7 +27,7 @@ class Config:
 
 
 def get_version() -> str:
-    return '0.13.4' if os.environ.get('DEV') is None else 'development'
+    return '0.14.0' if os.environ.get('DEV') is None else 'development'
 
 
 def _load() -> Config:
