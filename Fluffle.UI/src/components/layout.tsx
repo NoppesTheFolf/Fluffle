45fadca08c5d4adc45399a921e193f40d0914d03
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2022-02-14 19:41:41 +0100

    Fix Cloudflare flashing the 404 page before the routing module gets loaded

--- Fluffle.UI/src/components/layout.tsx
+++ Fluffle.UI/src/components/layout.tsx
@@ -33,7 +33,7 @@ const Layout = ({ center, title, maxWidth, requireBrowser, children }) => {
 
     return (
         <div className="w-full min-h-full flex flex-col">
-            <Helmet title={`${title} - Fluffle`}>
+            <Helmet title={title == null ? 'Fluffle' : `${title} - Fluffle`}>
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
--- Fluffle.UI/src/pages/404.js
+++ Fluffle.UI/src/pages/404.js
@@ -2,12 +2,21 @@ import * as React from 'react'
 import Layout from '../components/layout'
 
 const NotFoundPage = () => {
+    // This fixes Cloudflare Pages flashing a 404 screen...
+    const [shouldLeaveEmpty, setShouldLeaveEmpty] = React.useState(true);
+
+    React.useEffect(() => {
+        setShouldLeaveEmpty(window.location.pathname.startsWith('/mg/'));
+    }, []);
+
     return (
-        <Layout center={true} title="Not found">
-            <div className="flex flex-col items-center text-center">
-                <div className="text-4xl sm:text-7xl font-light mb-4">Blimey! 404 Not Found</div>
-                <div className="text-muted text-xl sm:text-3xl font-light">We couldn't find the page you're looking for</div>
-            </div>
+        <Layout center={true} title={shouldLeaveEmpty ? null : "Not found"}>
+            {!shouldLeaveEmpty &&
+                <div className="flex flex-col items-center text-center">
+                    <div className="text-4xl sm:text-7xl font-light mb-4">Blimey! 404 Not Found</div>
+                    <div className="text-muted text-xl sm:text-3xl font-light">We couldn't find the page you're looking for</div>
+                </div>
+            }
         </Layout>
     )
 }
