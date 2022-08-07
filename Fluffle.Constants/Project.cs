84fefc365593ba518540100956a1debf477fbbea
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2022-08-07 21:21:11 +0200

    Bump version to 0.17.0

--- Fluffle.Constants/Project.cs
+++ Fluffle.Constants/Project.cs
@@ -7,7 +7,7 @@ namespace Noppes.Fluffle.Constants
         /// <summary>
         /// Version of Fluffle.
         /// </summary>
-        public static string Version => Debugger.IsAttached ? "development" : "0.16.6";
+        public static string Version => Debugger.IsAttached ? "development" : "0.17.0";
 
         /// <summary>
         /// Base name of the User Agent used by Fluffle its synchronization and indexing bots.
