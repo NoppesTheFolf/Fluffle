07d8bff2f44595a4503e4c43920f9881969183cd
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2022-10-09 15:02:36 +0200

    Bump version to 0.18.0

--- Fluffle.Constants/Project.cs
+++ Fluffle.Constants/Project.cs
@@ -7,7 +7,7 @@ namespace Noppes.Fluffle.Constants
         /// <summary>
         /// Version of Fluffle.
         /// </summary>
-        public static string Version => Debugger.IsAttached ? "development" : "0.17.2";
+        public static string Version => Debugger.IsAttached ? "development" : "0.18.0";
 
         /// <summary>
         /// My username.
