97ed4e708dd6573bdaf37fe4a5e82fcb710d70f4
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2022-12-01 20:53:43 +0100

    Add DeviantArt database migration script

---
+++ Fluffle.DeviantArt.Database/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace Noppes.Fluffle.DeviantArt.Database;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var context = new DeviantArtDesignTimeContext().CreateDbContext(args);
+
+        Console.Write("Applying migrations...");
+        await context.Database.MigrateAsync();
+        Console.WriteLine(" OK!");
+    }
+}
