534233ece23a4cb1156831b4ca11863c02369abe
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2023-12-28 20:50:52 +0100

    Add basic benchmark for similarity service nearest neighbors method

---
+++ Fluffle.Search.Business.Benchmarks/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Noppes.Fluffle.Search.Business.Benchmarks;
+
+internal class Program
+{
+    private static void Main()
+    {
+        BenchmarkRunner.Run<SimilarityServiceNearestNeighborsBenchmark>();
+    }
+}
