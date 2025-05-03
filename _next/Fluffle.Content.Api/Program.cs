82d6d7e542a9431e87934880ef47b97b427e1564
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-05-03 12:36:47 +0200

    Use concurrency limiter in Content API

--- _next/Fluffle.Content.Api/Program.cs
+++ _next/Fluffle.Content.Api/Program.cs
@@ -1,4 +1,7 @@
 using Fluffle.Content.Api.Storage;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -10,9 +13,32 @@ services.AddOptions<FtpStorageOptions>()
 services.AddSingleton<FtpClientPool>();
 services.AddSingleton<FtpStorage>();
 
+services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor;
+});
+
+services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpContext =>
+        RateLimitPartition.GetConcurrencyLimiter(
+            partitionKey: httpContext.Connection.RemoteIpAddress?.ToString() ?? "unknown",
+            factory: _ => new ConcurrencyLimiterOptions
+            {
+                PermitLimit = 2,
+                QueueLimit = 50,
+                QueueProcessingOrder = QueueProcessingOrder.OldestFirst
+            }));
+    options.RejectionStatusCode = (int)HttpStatusCode.TooManyRequests;
+});
+
 services.AddControllers();
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
+
+app.UseRateLimiter();
 
 app.MapControllers();
 
