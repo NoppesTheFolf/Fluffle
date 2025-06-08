160b920a78458645d11b22a502bedd952629ec52
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-06-08 20:59:42 +0200

    Add integration tests for the Imaging API

---
+++ _next/Fluffle.Imaging.Api.IntegrationTests/SetUp.cs
@@ -0,0 +1,37 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using DotNet.Testcontainers.Images;
+using System.Net;
+
+namespace Fluffle.Imaging.Api.IntegrationTests;
+
+[SetUpFixture]
+public class SetUp
+{
+    private static readonly IFutureDockerImage ApiImage = new ImageFromDockerfileBuilder()
+        .WithDockerfileDirectory(CommonDirectoryPath.GetSolutionDirectory(), string.Empty)
+        .WithDockerfile("./Fluffle.Imaging.Api/Dockerfile")
+        .Build();
+
+    private static readonly IContainer ApiContainer = new ContainerBuilder()
+        .WithImage(ApiImage)
+        .WithPortBinding(1080, 8080)
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(x => x.ForPort(8080).ForStatusCode(HttpStatusCode.Unauthorized)))
+        .WithBindMount(Path.Join(CommonDirectoryPath.GetSolutionDirectory().DirectoryPath, "Fluffle.Imaging.Api/appsettings.Integration.json"), "/app/appsettings.json", AccessMode.ReadOnly)
+        .Build();
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetUpAsync()
+    {
+        await ApiImage.CreateAsync();
+        await ApiContainer.StartAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDownAsync()
+    {
+        await ApiContainer.DisposeAsync();
+        await ApiImage.DisposeAsync();
+    }
+}
---
+++ _next/Fluffle.Imaging.Api.IntegrationTests/Tests.cs
@@ -0,0 +1,70 @@
+﻿using DotNet.Testcontainers.Builders;
+using Fluffle.Imaging.Api.Client;
+using Fluffle.Imaging.Api.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System.Text.Json;
+
+namespace Fluffle.Imaging.Api.IntegrationTests;
+
+public class Tests
+{
+    private ServiceProvider _serviceProvider;
+    private IImagingApiClient _imagingApiClient;
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        _serviceProvider = new ServiceCollection()
+            .AddSingleton<IConfiguration>(new ConfigurationBuilder()
+                .AddInMemoryCollection([
+                    new KeyValuePair<string, string?>("ImagingApiClient:Url", "http://127.0.0.1:1080"),
+                    new KeyValuePair<string, string?>("ImagingApiClient:ApiKey", "ooghahfeig9yiThaghozu4no7kae9Pho")
+                ])
+                .Build())
+            .AddImagingApiClient()
+            .BuildServiceProvider();
+
+        _imagingApiClient = _serviceProvider.GetRequiredService<IImagingApiClient>();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDownAsync()
+    {
+        await _serviceProvider.DisposeAsync();
+    }
+
+    [TestCase("ColorSpaceCmyk", "jpg", 300, 95, true)]
+    [TestCase("Metadata", "jpg", 300, 95, true)]
+    [TestCase("OrientationLeftBottom", "jpg", 300, 95, true)]
+    [TestCase("Jpeg", "jpg", 500, 75, false)]
+    [TestCase("WebP", "webp", 600, 75, true)]
+    [TestCase("Png", "png", 400, 75, true)]
+    [TestCase("Gif", "gif", 300, 75, true)]
+    public async Task Test(string name, string inputExtension, int size, int quality, bool calculateCenter)
+    {
+        var testCasePath = Path.Join(CommonDirectoryPath.GetProjectDirectory().DirectoryPath, "TestCases", name);
+        var inputImagePath = Path.Join(testCasePath, $"input.{inputExtension}");
+        var inputImageMetadataPath = Path.Join(testCasePath, "input.json");
+
+        await using var inputImageStream1 = File.OpenRead(inputImagePath);
+        var expectedInputImageMetadataJson = await File.ReadAllTextAsync(inputImageMetadataPath);
+        var expectedInputImageMetadata = JsonSerializer.Deserialize<ImageMetadataModel>(expectedInputImageMetadataJson, JsonSerializerOptions.Web);
+        var inputImageMetadata = await _imagingApiClient.GetMetadataAsync(inputImageStream1);
+        inputImageMetadata.ShouldBeEquivalentTo(expectedInputImageMetadata);
+
+        var expectedImagePath = Path.Join(testCasePath, "expected.jpg");
+        var expectedImageMetadataPath = Path.Join(testCasePath, "expected.json");
+
+        await using var inputImageStream2 = File.OpenRead(inputImagePath);
+        var (actualImage, actualImageMetadata) = await _imagingApiClient.CreateThumbnailAsync(inputImageStream2, size, quality, calculateCenter);
+
+        var expectedImage = await File.ReadAllBytesAsync(expectedImagePath);
+        actualImage.ShouldBe(expectedImage);
+
+        var expectedImageMetadataJson = await File.ReadAllTextAsync(expectedImageMetadataPath);
+        var expectedImageMetadata = JsonSerializer.Deserialize<ImageMetadataModel>(expectedImageMetadataJson, JsonSerializerOptions.Web);
+        actualImageMetadata.ShouldBeEquivalentTo(expectedImageMetadata);
+    }
+}
