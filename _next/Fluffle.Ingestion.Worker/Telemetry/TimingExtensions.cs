f02c200382a316a3e444de0d60d9b1d6507ea307
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-06-21 16:09:37 +0200

    Track metrics for steps in Ingestion Worker

---
+++ _next/Fluffle.Ingestion.Worker/Telemetry/TimingExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.ApplicationInsights;
+using System.Diagnostics;
+
+namespace Fluffle.Ingestion.Worker.Telemetry;
+
+public static class TimingExtensions
+{
+    public static async Task Timed(this Task task, TelemetryClient telemetryClient, string name)
+    {
+        var metric = telemetryClient.GetMetric(name);
+        var stopwatch = Stopwatch.StartNew();
+        try
+        {
+            await task;
+        }
+        finally
+        {
+            metric.TrackValue(stopwatch.ElapsedMilliseconds);
+        }
+    }
+
+    public static async Task<T> Timed<T>(this Task<T> task, TelemetryClient telemetryClient, string name)
+    {
+        var metric = telemetryClient.GetMetric(name);
+        var stopwatch = Stopwatch.StartNew();
+        try
+        {
+            var result = await task;
+            return result;
+        }
+        finally
+        {
+            metric.TrackValue(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
