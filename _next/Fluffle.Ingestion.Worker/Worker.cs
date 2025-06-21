8de4f18126c09c050af26510ea6307436fb79626
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-06-21 16:16:04 +0200

    Also track metrics for Ingestion API calls in Ingestion Worker

--- _next/Fluffle.Ingestion.Worker/Worker.cs
+++ _next/Fluffle.Ingestion.Worker/Worker.cs
@@ -1,5 +1,7 @@
 using Fluffle.Ingestion.Api.Client;
 using Fluffle.Ingestion.Worker.ItemActionHandlers;
+using Fluffle.Ingestion.Worker.Telemetry;
+using Microsoft.ApplicationInsights;
 using Microsoft.Extensions.Options;
 using System.Runtime.CompilerServices;
 
@@ -11,17 +13,20 @@ public class Worker : BackgroundService
     private readonly ItemActionHandlerFactory _itemActionHandlerFactory;
     private readonly IOptions<WorkerOptions> _options;
     private readonly ILogger<Worker> _logger;
+    private readonly TelemetryClient _telemetryClient;
 
     public Worker(
         IIngestionApiClient ingestionApiClient,
         ItemActionHandlerFactory itemActionHandlerFactory,
         IOptions<WorkerOptions> options,
-        ILogger<Worker> logger)
+        ILogger<Worker> logger,
+        TelemetryClient telemetryClient)
     {
         _ingestionApiClient = ingestionApiClient;
         _itemActionHandlerFactory = itemActionHandlerFactory;
         _options = options;
         _logger = logger;
+        _telemetryClient = telemetryClient;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -44,7 +49,7 @@ public class Worker : BackgroundService
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var itemAction = await _ingestionApiClient.DequeueItemActionAsync();
+            var itemAction = await _ingestionApiClient.DequeueItemActionAsync().Timed(_telemetryClient, "IngestionApiDequeueItemAction");
             if (itemAction != null)
             {
                 using (_logger.BeginScope("ItemActionId:{ItemActionId} ItemId:{ItemId}", itemAction.ItemActionId, itemAction.ItemId))
@@ -55,7 +60,7 @@ public class Worker : BackgroundService
                     {
                         await handler.RunAsync();
 
-                        await _ingestionApiClient.AcknowledgeItemActionAsync(itemAction.ItemActionId);
+                        await _ingestionApiClient.AcknowledgeItemActionAsync(itemAction.ItemActionId).Timed(_telemetryClient, "IngestionApiAcknowledgeItemAction");
                     }
                     catch (Exception e)
                     {
