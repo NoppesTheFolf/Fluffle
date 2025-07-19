dd92d5953db180144511491b74410db9873446c7
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-07-19 13:04:10 +0200

    Add exception tracking to feeders

---
+++ _next/Fluffle.Feeder.Framework/ApplicationInsights/ApplicationInsightsOptions.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Fluffle.Feeder.Framework.ApplicationInsights;
+
+internal class ApplicationInsightsOptions
+{
+    public const string ApplicationInsights = "ApplicationInsights";
+
+    [Required]
+    public required string ConnectionString { get; set; }
+
+    [Required]
+    public required string CloudRoleName { get; set; }
+}
