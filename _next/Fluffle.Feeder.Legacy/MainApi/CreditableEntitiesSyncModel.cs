fdae7d90bec9b8170794ae086cbf79a1a6a0002a
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-08-30 13:02:21 +0200

    Remove legacy feeder

--- _next/Fluffle.Feeder.Legacy/MainApi/CreditableEntitiesSyncModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fluffle.Feeder.Legacy.MainApi;
-
-public class CreditableEntitiesSyncModel
-{
-    public long NextChangeId { get; set; }
-
-    public ICollection<CreditableEntityModel> Results { get; set; } = null!;
-}
--- _next/Fluffle.Feeder.Legacy/MainApi/ImageSyncModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fluffle.Feeder.Legacy.MainApi;
-
-public class ImagesSyncModel
-{
-    public long NextChangeId { get; set; }
-
-    public ICollection<ImageModel> Results { get; set; } = null!;
-}
