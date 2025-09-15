bf55a56703b7c3c520112a5311562c8c08a794f6
NoppesTheFolf <48657170+NoppesTheFolf@users.noreply.github.com>
2025-09-15 19:18:24 +0200

    Handle odd Weasyl case where a submission has multiple images

--- _next/Fluffle.Feeder.Weasyl/WeasylSubmissionExtensions.cs
+++ _next/Fluffle.Feeder.Weasyl/WeasylSubmissionExtensions.cs
@@ -7,11 +7,19 @@ internal static class WeasylSubmissionExtensions
 {
     public static IEnumerable<ImageModel> GetImages(this WeasylSubmission submission)
     {
+        // The weird checking below has to do with https://www.weasyl.com/~kashmere/submissions/318979/husky-tough
+        // That submissions contains more than a single image in the cover/submission fields, but refer to the same file
         if (submission.Media.Cover != null)
         {
+            var uniqueCoversCount = submission.Media.Cover.Select(x => x.Url.AbsoluteUri).Distinct().Count();
+            if (uniqueCoversCount > 1)
+            {
+                throw new InvalidOperationException($"Submission with ID {submission.SubmitId} contains more than a single cover media.");
+            }
+
             yield return new ImageModel
             {
-                Url = submission.Media.Cover.Single().Url.AbsoluteUri,
+                Url = submission.Media.Cover.First().Url.AbsoluteUri,
                 Width = 1000,
                 Height = 1000
             };
@@ -19,9 +27,15 @@ internal static class WeasylSubmissionExtensions
 
         if (submission.Media.Submission != null)
         {
+            var uniqueSubmissionsCount = submission.Media.Submission.Select(x => x.Url.AbsoluteUri).Distinct().Count();
+            if (uniqueSubmissionsCount > 1)
+            {
+                throw new InvalidOperationException($"Submission with ID {submission.SubmitId} contains more than a single submission media.");
+            }
+
             yield return new ImageModel
             {
-                Url = submission.Media.Submission.Single().Url.AbsoluteUri,
+                Url = submission.Media.Submission.First().Url.AbsoluteUri,
                 Width = 2000,
                 Height = 2000
             };
